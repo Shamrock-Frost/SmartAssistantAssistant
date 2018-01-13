186998ac98495df2cfd9894a25f8e495a92913a8
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-01-13 06:18:39 -0800

    Added initial #%module-begin

---
+++ main.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(require syntax/parse/define)
+(provide #%app #%datum 
+         (rename-out [saa-module-begin #%module-begin]))
+
+(define-syntax-parser saa-module-begin
+  [(_ prog ...) #'(#%module-begin prog ...)])
