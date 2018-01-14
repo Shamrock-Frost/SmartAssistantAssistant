f91fd5a940bedd516fe37ecfd4a1f6c958771524
Brendan Seamas Murphy <shamrockfrost@gmail.com>
2018-01-14 00:54:50 -0800

    Added define-handler/intent-request

--- saa-server.rkt
+++ saa-server.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require web-server/http web-server/http/request-structs syntax/parse/define)
+(require web-server/http syntax/parse/define)
 (provide (all-from-out racket))
 
 (define-syntax-parser define-handler/launch-request
@@ -10,3 +10,20 @@
        (define time (hash-ref req-obj "timestamp"))
        (define locale (hash-ref req-obj "locale"))
        body ...)])
+
+(define-syntax-parser define-handler/intent-request
+  [(_ (name:id intent-name:id value-map:id) body:expr ...+)
+   #'(define (name req-obj)
+       (cond
+         [(hash-has-key? req-obj "type")
+          (define intent (hash-ref req-obj "intent"))
+          (define intent-name (hash-ref intent "name"))
+          (define slots (hash-ref intent "slots"))
+          (define value-map (for/hash ([(slot-name slot-obj) slots])
+                              (values slot-name (hash-ref (hash-ref (first (hash-ref (first (hash-ref (hash-ref slot-obj "resolutions") "resultionsPerAuthority")) "values")) "value") "name"))))
+          body ...]
+         [(hash-has-key? req-obj "user")
+          (define inputs (hash-ref req-obj "inputs"))
+          (define intent-name (hash-ref input "intent"))
+          (define value-map (for/hash ([arg-obj (hash-ref input "arguments")])
+                              (values (hash-ref arg-obj "name") (hash-ref arg-obj "textValue"))))]))])
